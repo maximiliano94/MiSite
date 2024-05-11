2dbd3831fd95e51bb154dd89fce602ed7a037497
Maximiliano Jara <33096329+maximiliano94@users.noreply.github.com>
2024-05-10 20:23:28 -0400

    version final

---
+++ app.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", () => {
+    document.querySelector(".menu-btn").addEventListener("click", () => {
+        document.querySelector(".nav-menu").classList.toggle("show");
+    });
+});
+
+
+// SCROLL REVEAL
+
